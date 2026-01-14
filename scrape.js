7a6b4a158de8b893e81ce008cf32cc8106088ff4
reisz1995 <150909903+reisz1995@users.noreply.github.com>
2026-01-14 02:21:26 -0300

    Atualizar o scrape.js

--- scrape.js
+++ scrape.js
@@ -1,5 +1,5 @@
 import fetch from "node-fetch";
-import cheerio from "cheerio";
+import * as cheerio from "cheerio";
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
